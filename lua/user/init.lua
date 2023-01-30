aa2da327eb1ac7eb6425d3deee8fc8cfa7362f4c
Ismet Handzic <ismet55555@users.noreply.github.com>
2023-01-29 22:15:08 -0500

    Update init.lua

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -318,14 +318,14 @@ local config = {
 --           wilder.set_option('renderer', wilder.popupmenu_renderer({
 --             pumblend = 20,
 --           }))
---           wilder.set_option('renderer', wilder.popupmenu_renderer(
---             wilder.popupmenu_border_theme({
---               highlights = {
---                 border = 'Normal', -- highlight to use for the border
---               },
---               border = 'rounded',
---             })
---           ))
+          wilder.set_option('renderer', wilder.popupmenu_renderer(
+            wilder.popupmenu_border_theme({
+              highlights = {
+                border = 'Normal', -- highlight to use for the border
+              },
+              border = 'rounded',
+            })
+          ))
         end,
       },
 
@@ -359,7 +359,7 @@ local config = {
       return config -- return final config table
     end,
     treesitter = { -- overrides `require("treesitter").setup(...)`
-      ensure_installed = { "lua", "python", "bash", "yaml" },
+      ensure_installed = { "lua" },
     },
     
     telescope = {
