8a950fa3d51bf66695b1421c21aedd504eee8811
Ismet Handzic <ismet55555@users.noreply.github.com>
2023-01-30 00:35:54 -0500

    Update init.lua

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -312,9 +312,9 @@ local config = {
           wilder.setup {
             modes = {":", "/", "?"},
           }
-          wilder.set_option('renderer', wilder.wildmenu_renderer({
-            highlighter = wilder.basic_highlighter(),
-          }))
+--           wilder.set_option('renderer', wilder.wildmenu_renderer({
+--             highlighter = wilder.basic_highlighter(),
+--           }))
           wilder.set_option('renderer', wilder.popupmenu_renderer({
             pumblend = 20,
           }))
